84cf6eccbca19006c4a0b7bfef4f70fd12681831
Daniel Leong <me@dhleong.net>
2019-03-12 20:08:29 -0400

    Just use the 'uninstall' event to trigger restart
    Duh

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ if (
     case "restart":
         console.log("Stopping service, and ...");
         svc.stop();
-        svc.uninstall();
 
-        setTimeout(() => {
-            console.log("... restarting...");
+        svc.once('uninstall', function() {
+            console.log('... restarting...');
             svc.install();
-        }, 250);
+        });
+        svc.uninstall();
         break;
 
     case "start":
