de0f2888a281a9955a8a256b3845ec8e5dbcdaf2
Daniel Leong <me@dhleong.net>
2022-07-31 15:51:38 -0400

    Monkey-patch node-mac to improve robustness

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const { EventEmitter } = require('stream');
 const execFile = require('util').promisify(require('child_process').execFile);
 
 function onServiceAsPromise(svc, method) {
@@ -14,10 +15,33 @@ function onServiceAsPromise(svc, method) {
     });
 }
 
-async function startServiceMacos() {
+function createService(config) {
+    // NOTE: This lib is quite janky. We should probably replace it, but for now we
+    // monkey patch certain methods that are not robust
     const Service = require('node-mac').Service;
+    const original = new Service(config);
 
-    const svc = new Service({
+    // NOTE: We have to go through some quite *extra* work to monkey-patch,
+    // since some properties are mysteriously not enumerable, and most are
+    // not writable (although some are...?!)
+    const patched = new Proxy(new EventEmitter(), {
+        get(target, prop, receiver) {
+            // Some properties are mysteriously not enumerable...
+            return target[prop] ?? original[prop];
+        },
+    });
+
+    patched.stop = async (callback) => {
+        await execFile('launchctl', ['unload', patched.plist]);
+        patched.emit('stop');
+        callback && callback();
+    };
+
+    return patched;
+}
+
+async function startServiceMacos() {
+    const svc = createService({
         name: 'MyHome',
         description: 'My Smarthome server',
         script: require('path').join(__dirname, 'my-home.js'),
@@ -54,15 +78,8 @@ async function startServiceMacos() {
 
     switch (command) {
     case "restart":
-        console.log("Stopping service, and ...");
-        // NOTE: node-mac will throw an error when we try to stop() if the log file
-        // doesn't exist, so we do it ourselves...
-        // await onServiceAsPromise(svc, "stop");
-        await execFile('launchctl', ['unload', svc.plist]);
-
-        // Delete the log dir so node-mac doesn't try to do that itself (and potentially fail)
-        console.log("... uninstalling, and ...");
-        fs.rmdirSync(LOG_DIR, { recursive: true, force: true });
+        // NOTE: uninstall also calls stop()
+        console.log("Uninstalling service, and ...");
         await onServiceAsPromise(svc, "uninstall");
 
         console.log("... restarting...");
@@ -87,7 +104,10 @@ if (
 ) {
     startServiceMacos()
         .then(() => console.log("Done!"))
-        .catch(e => { throw e; });
+        .catch(e => {
+            console.error(e);
+            process.exit(1);
+        });
 } else {
     require("./my-home");
 }
